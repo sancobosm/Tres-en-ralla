93de03158850f510e423d9cc7fdaa604d613335c
1321700 <1321700@alu.murciaeduca.es>
2022-12-30 09:40:26 +0100

    Commit inicial

---
+++ app/src/main/java/cobos/santiago/tresenralla/MainActivity.java
@@ -0,0 +1,14 @@
+package cobos.santiago.tresenralla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
